9e9bef4ec1a884ff3f69c7d1936571eaa7ee669c
Petr Lobachev <petya.lobachev@yandex.ru>
2021-06-04 20:49:21 +0300

    feat: добавить mongoose и подключить бд

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-
-const {PORT} = require('./config/index')
-
-
+const express = require('express');
+const mongoose = require('mongoose');
+const { PORT, MONGO_URL } = require('./config');
 
 const app = express();
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
-app.listen(PORT, ()=> {
-    console.log(`App listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
-const  {
-    PORT = 3002,
-} = process.env
+const {
+  PORT = 3002,
+  MONGO_URL = 'mongodb://localhost:27017/newbornPhotographerdb',
+} = process.env;
 
-module.exports  = {PORT}
+module.exports = { PORT, MONGO_URL };

28801c31ac18593046a429346029736ffe65179f
Petr Lobachev <petya.lobachev@yandex.ru>
2021-09-02 23:25:59 +0300

    feat: добавить CORS

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const { PORT, MONGO_URL } = require('./config');
 
 const app = express();
+const router = require('./routes/index');
+
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);

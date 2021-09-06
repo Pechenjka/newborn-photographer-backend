c69ad719336002276a86448ece1e7d35dd07b965
Petr Lobachev <petya.lobachev@yandex.ru>
2021-09-06 20:42:37 +0300

    feat: вынести текст ошибок в отдельный файл

---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+const UNAUTHORIZED_MESSAGE = 'Необходимо обновить авторизацию или заново авторизоваться';
+const DEFAULT_ERROR_SERVER_MESSAGE = 'На сервере произошла ошибка';
+const NOT_FOUND_MESSAGE = 'Бакет не найден';
+const BAD_REQUEST_MESSAGE = 'Некорректный запрос';
+
+module.exports = {
+  UNAUTHORIZED_MESSAGE, DEFAULT_ERROR_SERVER_MESSAGE, NOT_FOUND_MESSAGE, BAD_REQUEST_MESSAGE,
+};
